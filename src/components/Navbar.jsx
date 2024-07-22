250c1cc8d884d94c6e50c975dda7b9e9abc8b17e
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-07-22 13:41:21 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,26 @@
+import React from 'react'
+import styles from '../styles/Navbar.module.css';
 
+const Navbar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <h1 className={styles.logo}>Samsung</h1>
+            <ul className={styles.navlinks}>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
